3b1c0cedc1bbca4da03c68967bf89aca60396da7
pranjal-13 <69795266+pranjal-13@users.noreply.github.com>
2021-03-24 11:42:25 +0530

    Create TestRunner2.java

---
+++ src/test/java/Runner/TestRunner2.java
@@ -0,0 +1,20 @@
+package Runner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions( 
+		features="src/test/resources/Features/TC03-NegativePaymentValidation1.feature"
+		,glue= {"StepDefinition"}
+		,monochrome=true
+		,dryRun=false
+//		,tags="@TC_01_01_ImportMessage"
+		,plugin = {"pretty", "html:target/cucumber-html-report","json:target/cucumber-reports/Cucumber.json","junit:target/cucumber-reports/Cucumber.xml",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+
+		)
+public class TestRunner3 {
+
+}
